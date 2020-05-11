3bc7f158e67dc170304629e883be35a41782c777
higorcos <63820192+higorcos@users.noreply.github.com>
2020-05-11 17:50:23 -0300

    site

---
+++ Curso Js/Aulas/aula11.js
@@ -0,0 +1,8 @@
+/////////////condições 
+var vel = 60.5
+
+if(vel > 60){
+console.log(`Sua velocidade é ${vel} voçe está acima do velocidade`);
+} else {
+    console.log(`Dirija com cuidado `)
+}
---
+++ Curso Js/Aulas/aula13.js
@@ -0,0 +1,12 @@
+////loços
+/*n=0;
+while(n != 1000000){
+ console.log(`Passo ${n} `);
+ n += 1;
+}*/
+
+n=0;
+do {
+n ++;
+console.log(`Passo ${n}`);
+}while(n!= 10)
