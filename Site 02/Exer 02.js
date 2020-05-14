56e5b273a0ca1d6038f3f59992a53e50d89dd914
higorcos <63820192+higorcos@users.noreply.github.com>
2020-05-14 00:10:11 -0300

    Add files via upload

---
+++ Site 02/Exer 02.js
@@ -0,0 +1,57 @@
+function verificar() {
+    var data = new Date();
+    var ano = data.getFullYear();
+
+    var fAno = window.document.getElementById("iAno");
+
+    var resuldado = window.document.querySelector("div#iResultado")
+
+    if (fAno.value.length == 0 || fAno.value > ano || fAno.value == 0) {
+        window.alert("Verifique os dados e tente novamente! ");
+    } else {
+        var sexo = window.document.getElementsByName('nSexo');
+        var idade = ano - Number(fAno.value);
+        var genero = '';
+        var img = document.createElement('img'); ///criando html no java script
+        img.setAttribute('id', 'img01'); //criamos um id para a fotochamada img1
+
+        if (sexo[0].checked) {
+            genero = 'Homem';
+            if (idade > 0 && idade < 11) {
+                //criança
+                img.setAttribute('src','https://i.ibb.co/7XPYYzf/Crian-a-h.png');
+            } else if (idade < 22) {
+                //jovem
+                img.setAttribute('src','https://i.ibb.co/31CFBDc/Jovens-h.png');
+            } else if (idade < 60) {
+                //Adulto
+                img.setAttribute('src','https://i.ibb.co/0QRZLZx/Adulto-h.png');
+            } else {
+                //idoso
+                img.setAttribute('src','https://i.ibb.co/dLJmvsD/Idoso-h.png');
+            }
+
+        } else if (sexo[1].checked) {
+            genero = 'Mulher';
+            if (idade > 0 && idade < 11) {
+                //criança
+                img.setAttribute('src','https://i.ibb.co/0XpK2VT/Crian-a-m.png');
+            } else if (idade < 22) {
+                //jovem
+                img.setAttribute('src','https://i.ibb.co/z2wPj2R/Jovens-m.png');
+            } else if (idade < 60) {
+                //Adulto
+                img.setAttribute('src','https://i.ibb.co/zf18t4b/Adulto-m.png');
+            } else {
+                //idoso
+                img.setAttribute('src','https://i.ibb.co/ZmSxxRk/Idoso-m.png');
+            }
+        }
+        resuldado.style.textAlign = 'center';
+        resuldado.innerHTML = ` ${genero} de ${idade} anos`
+        resuldado.innerHTML += `</br>`
+
+        resuldado.appendChild(img);
+    }
+}
+
---
+++ Site 03/Exer 03.js
@@ -0,0 +1,45 @@
+
+function contar() {
+    let numeroI = Number(document.getElementById('iInicial').value);
+    let numeroF = Number(document.getElementById('iFinal').value);
+    let pulo = Number(document.getElementById('iPulo').value);
+
+    let resultado = document.getElementById('iRe');
+
+    console.log(numeroI)
+    console.log(numeroF)
+    console.log(pulo)
+
+
+    if (numeroI == 0 || numeroF == 0 ) {
+        resultado.innerHTML = `[Erro] selecione os números`
+    } else {
+        resultado.innerHTML = `Contando \u{1F916} `;
+    if(pulo <= 0 ){
+        pulo = 1;
+        window.alert('O pulo será igua a 1');
+    }
+    if(numeroF > numeroI){
+        for(let n = numeroI; n <= numeroF; n += pulo) {
+            console.log(n);
+            resultado.innerHTML += `\u{1F449}${n}; `
+            
+        }
+        resultado.innerHTML += `\u{1F389}`
+    }else {
+            for(let n = numeroI; n >= numeroF; n -= pulo) {
+                console.log(n);
+                resultado.innerHTML += `\u{1F449}${n}; `
+                
+            }
+            resultado.innerHTML += `\u{1F389}`
+    
+    }
+
+}
+}
+
+
+
+
+
