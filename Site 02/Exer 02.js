dfb85b6a5eb9abcf87ee091c53d12ebc51cecba9
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2020-09-10 15:55:02 -0300

    Melhorando a contagem dos meses sem uso de biblioteca

--- Site 02/Exer 02.js
+++ Site 02/Exer 02.js
@@ -1,130 +1,106 @@
-    var dateNow = new Date();  // puxa a data 
-
-    var dayNow = dateNow.getDay()
-    var monthNow = (dateNow.getMonth()+ 1) //o mês começa em 0
-    var yearNow = dateNow.getFullYear()
-    //pega o mês do cliente, recebe o mes indicado de 0 a 11
-    //pega o ano do cliente 
+var dateNow = new Date();  // puxa a data 
+var dayNow = dateNow.getDay() // puxa o dia
+var monthNow = (dateNow.getMonth() + 1) //puxa o  mês que começa em 0
+var yearNow = dateNow.getFullYear() //puxa o ano 
 
 const verificar = () => {
     //monthF();
     //console.log(date);
 
     var AnoDoNascimento = (window.document.getElementById("iAno").value); //AAAA-MM-DD Formato da data fornecida pelo usuário
-    
-    const day = AnoDoNascimento.split('-')[2]; // dia
-    const month = AnoDoNascimento.split('-')[1]; //  nesse caso não precisamos fazer nenhum filtro diante do valor do mês
-    const yearNas = AnoDoNascimento.split('-')[0]; // ano
-    console.log(day+ "/" + month + "/" + yearNas) 
-    
 
-    var resuldado = window.document.querySelector("div#iResultado")  
+    const day = AnoDoNascimento.split('-')[2];     // dia
+    const month = AnoDoNascimento.split('-')[1];   //nesse caso não precisamos fazer nenhum filtro diante do valor do mês
+    const yearNas = AnoDoNascimento.split('-')[0]; // ano
+    console.log(day + "/" + month + "/" + yearNas) //mostra a data no formato do Brasil
+
+
+
+    var resuldado = window.document.querySelector("div#iResultado")
 
     if (yearNas.length == 0 || yearNas > yearNow || yearNas == 0) {
         window.alert("Verifique os dados e tente novamente! ");
     } else {
         var sexo = window.document.getElementsByName('nSexo');
         var idade = yearNow - yearNas;
+        var diferençaMeses = monthNow - month 
+        var anos = '';
         var genero = '';
         var img = document.createElement('img'); //criando html pelo javascript
         img.setAttribute('id', 'img01'); //criamos um id para a foto chamada img1
 
+        let testeN = Math.sign(diferençaMeses) //Verifica se o número é positivo negativo ou neutro
+    console.log(testeN, "teste");
+
+if (testeN == -1){
+    diferençaMeses = (monthNow - month) + 12;
+    console.log(diferençaMeses);
+}else{ console.log(diferençaMeses)}
+
         if (sexo[0].checked) {
             genero = 'Homem';
-
-            if(yearNow == yearNas){
+            if (diferençaMeses < 13) {
                 genero = 'Bebê'
                 //bebê
-                img.setAttribute('src','https://i.ibb.co/HHBQCnr/beb.png')
-            }else if (idade > 0 && idade < 11) {
+                img.setAttribute('src', 'https://i.ibb.co/HHBQCnr/beb.png')
+                anos = 'meses'
+
+            } else if (idade > 0 && idade < 11) {
                 genero = 'Criança'
+                anos = 'anos'
                 //criança
-                img.setAttribute('src','https://i.ibb.co/7XPYYzf/Crian-a-h.png');
+                img.setAttribute('src', 'https://i.ibb.co/7XPYYzf/Crian-a-h.png');
             } else if (idade < 22) {
                 genero = 'Jovem'
+                anos = 'anos'
                 //jovem
-                img.setAttribute('src','https://i.ibb.co/31CFBDc/Jovens-h.png');
+                img.setAttribute('src', 'https://i.ibb.co/31CFBDc/Jovens-h.png');
             } else if (idade < 60) {
+                anos = 'anos'
                 //Adulto
-                img.setAttribute('src','https://i.ibb.co/0QRZLZx/Adulto-h.png');
+                img.setAttribute('src', 'https://i.ibb.co/0QRZLZx/Adulto-h.png');
             } else {
+                anos = 'anos'
                 //idoso
-                img.setAttribute('src','https://i.ibb.co/dLJmvsD/Idoso-h.png');
+                img.setAttribute('src', 'https://i.ibb.co/dLJmvsD/Idoso-h.png');
             }
 
         } else if (sexo[1].checked) {
             genero = 'Mulher';
-            if(yearNow == yearNas){
+            if (yearNow == yearNas) {
                 genero = 'Bebê'
                 //bebê
-                img.setAttribute('src','https://i.ibb.co/HHBQCnr/beb.png')
-            }else if (idade > 0 && idade < 11) {
+                anos = 'meses'
+                img.setAttribute('src', 'https://i.ibb.co/HHBQCnr/beb.png')
+            } else if (idade > 0 && idade < 11) {
                 genero = 'Criança'
+                anos = 'anos'
                 //criança
-                img.setAttribute('src','https://i.ibb.co/0XpK2VT/Crian-a-m.png');
+                img.setAttribute('src', 'https://i.ibb.co/0XpK2VT/Crian-a-m.png');
             } else if (idade < 22) {
                 genero = 'Jovem'
+                anos = 'anos'
                 //jovem
-                img.setAttribute('src','https://i.ibb.co/z2wPj2R/Jovens-m.png');
+                img.setAttribute('src', 'https://i.ibb.co/z2wPj2R/Jovens-m.png');
             } else if (idade < 60) {
+                anos = 'anos'
                 //Adulto
-                img.setAttribute('src','https://i.ibb.co/zf18t4b/Adulto-m.png');
+                img.setAttribute('src', 'https://i.ibb.co/zf18t4b/Adulto-m.png');
             } else {
+                anos = 'anos'
                 //idoso
-                img.setAttribute('src','https://i.ibb.co/ZmSxxRk/Idoso-m.png');
+                img.setAttribute('src', 'https://i.ibb.co/ZmSxxRk/Idoso-m.png');
             }
         }
         resuldado.style.textAlign = 'center';
-        resuldado.innerHTML = ` ${genero} de ${idade} anos`
+        resuldado.innerHTML = ` ${genero} de ${idade} ${anos}`
         resuldado.innerHTML += `</br>`
 
         resuldado.appendChild(img);    //atribui de fato a imagem 
     }
-    
+
 }
-const monthF = () => {
-    switch (monthNow) {
-        case 0: 
-            console.log("Janeiro");
-            break;
-        case 1:
-            console.log("Fevereiro");
-            break;
-        case 2: 
-            console.log("Março");
-            break;        
-        case 3: 
-            console.log("Abril");
-            break;        
-        case 4: 
-            console.log("Maio");
-            break;        
-        case 5: 
-            console.log("Junho");
-            break;        
-        case 6: 
-            console.log("Julho");
-            break;        
-        case 7: 
-            console.log("Agosto");
-            break;
-        case 8: 
-            console.log("Setembro");
-            break;
-        case 9: 
-            console.log("Outubro");
-            break;
-        case 10: 
-            console.log("Novembro");
-            break;
-        case 11: 
-            console.log("Dezembro");
-            break;
-        default:
-            console.log("Erro 404");
-            
-    }
-}
+
 
 
 
