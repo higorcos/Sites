c157d393dffc039b80a4aa4b9103888532946c8f
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2020-11-08 22:05:52 -0300

    Sucesso 0.0.1

--- Site 06/Exer 06.js
+++ Site 06/Exer 06.js
@@ -1,2 +1,23 @@
 //Ideia do projeto em julho 
 // icone mostrando o consumo de energia do mês
+
+const carregar = () => {
+    var msg = window.document.getElementById('msg');
+    
+    var horasDeUso = 0;
+    var usoDias = 0;
+    var tarifaEnergia = 0;
+    potenciaEquipamento = 40;
+    horasDeUso = 6;
+    usoDias = 20;
+    tarifaEnergia = 0.60;
+    
+    
+    var consumo = potenciaEquipamento * horasDeUso * usoDias /1000 
+    var preço = consumo * tarifaEnergia
+    msg.innerHTML = `O consumo mensal do aparelho  ${consumo} custo do funcinamento dele ${preço}`
+     
+
+    
+
+}
