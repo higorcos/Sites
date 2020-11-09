be18fe56cb77ba082bc131941fc6f5db97eec88c
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2020-11-08 22:05:02 -0300

    Reformulando

---
+++ Site 07/Exer 07.js
@@ -0,0 +1,22 @@
+
+
+const carregar = () => {
+    var msg = window.document.getElementById('msg');
+    
+    var horasDeUso = 0;
+    var usoDias = 0;
+    var tarifaEnergia = 0;
+    potenciaEquipamento = 40;
+    horasDeUso = 6;
+    usoDias = 20;
+    tarifaEnergia = 0.60;
+    
+    
+    var consumo = potenciaEquipamento * horasDeUso * usoDias /1000 
+    var preço = consumo * tarifaEnergia
+    msg.innerHTML = `O consumo mensal do aparelho  ${consumo} custo do funcinamento dele ${preço}`
+     
+
+    
+
+}
