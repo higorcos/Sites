906d5156156f793affd58a14e85ec7acd2b9c005
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2020-11-11 15:24:24 -0300

    Dadas do usuário

--- Site 06/Exer 06.js
+++ Site 06/Exer 06.js
@@ -2,22 +2,41 @@
 // icone mostrando o consumo de energia do mês
 
 const carregar = () => {
+
+
+
+
+
+
     var msg = window.document.getElementById('msg');
-    
+
     var horasDeUso = 0;
     var usoDias = 0;
     var tarifaEnergia = 0;
+    var potenciaEquipamento = 0;
+    
     potenciaEquipamento = 40;
     horasDeUso = 6;
     usoDias = 20;
     tarifaEnergia = 0.60;
-    
-    
-    var consumo = potenciaEquipamento * horasDeUso * usoDias /1000 
-    var preço = consumo * tarifaEnergia
-    msg.innerHTML = `O consumo mensal do aparelho  ${consumo} custo do funcinamento dele ${preço}`
-     
+
+    potenciaEquipamento = document.getElementById('iPotencia').value
+    horasDeUso = document.getElementById('iHoras').value
+    usoDias = document.getElementById('iDias').value
+    tarifaEnergia = document.getElementById('iTarifas').value
+console.log(potenciaEquipamento)
+console.log(horasDeUso)
+console.log(usoDias)
+console.log(tarifaEnergia)
+
 
     
+
+    var consumo = potenciaEquipamento * horasDeUso * usoDias / 1000
+    var preço = consumo * tarifaEnergia
+    msg.innerHTML = `O consumo mensal do aparelho  ${consumo} custo do funcinamento dele ${preço}`
+
+
+
 
 }
