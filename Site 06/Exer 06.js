cce2be87ea44e47121e2cbe7988047dafee52ab7
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2020-11-23 16:36:00 -0300

    Add files via upload

--- Site 06/Exer 06.js
+++ Site 06/Exer 06.js
@@ -1,38 +1,26 @@
 //Ideia do projeto em julho 
 // icone mostrando o consumo de energia do mês
 
+//verificar se foi colocado tarifa se não for falar para o usuário
+
 const carregar = () => {
+
     var msg = window.document.getElementById('msg');
 
-    var horasDeUso = 0;
-    var usoDias = 0;
-    var tarifaEnergia = 0;
-    var potenciaEquipamento = 0;
-    /*potenciaEquipamento = 40;
-    horasDeUso = 6;
-    usoDias = 20;
-    tarifaEnergia = 0.60;
-    */
-    potenciaEquipamento = document.getElementById('iPotencia').value
-    horasDeUso = document.getElementById('iHoras').value
-    usoDias = document.getElementById('iDias').value
-    tarifaEnergia = document.getElementById('iTarifas').value
+    let potenciaEquipamento = document.getElementById('iPotencia').value
+    let horasDeUso = document.getElementById('iHoras').value
+    let usoDias = document.getElementById('iDias').value
+    let tarifaEnergia = document.getElementById('iTarifas').value
     console.log(tarifaEnergia, "tarifa")
 
-
-    var consumo = (potenciaEquipamento * horasDeUso * usoDias / 1000).toFixed(2)
+    let consumo = (potenciaEquipamento * horasDeUso * usoDias / 1000).toFixed(2)
     console.log(consumo, "consumo")
  
-    let preço = 0;
-    preço = (consumo * tarifaEnergia).toFixed(2)
+    let preço = (consumo * tarifaEnergia).toFixed(2)
 
     console.log(preço, "preço")
     msg.innerHTML = `<div class="retornoJS">
     <p>Consumo mensal: ${consumo}</p> 
     <p>Custo mensal: ${preço}</p>
     </div>`
-
-
-//verificar se foi colocado tarifa se não for falar para o usuário
-
 }
