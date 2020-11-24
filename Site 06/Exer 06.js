9b1d2405d67923db10c91d834ee8fa1d82827045
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2020-11-24 10:48:12 -0300

    Validação 2.0

--- Site 06/Exer 06.js
+++ Site 06/Exer 06.js
@@ -1,30 +1,32 @@
 //Ideia do projeto em julho 
 // icone mostrando o consumo de energia do mês
-
-//verificar se foi colocado tarifa se não for falar para o usuário
-
 const carregar = () => {
-
     var msg = window.document.getElementById('msg');
 
     let potenciaEquipamento = document.getElementById('iPotencia').value
     let horasDeUso = document.getElementById('iHoras').value
     let usoDias = document.getElementById('iDias').value
     let tarifaEnergia = document.getElementById('iTarifas').value
-    if(tarifaEnergia == ""){
+    if (potenciaEquipamento.length == 0) {
+        alert("Adicione a Potência do equipamento")
+    } else if (horasDeUso.length == 0) {
+        alert("Adicione o total de horas de uso diário do equipamento")
+    } else if (usoDias.length == 0) {
+        alert("Adicione o total de dias de uso durante o periodo de um mês")
+    }
+    else if (tarifaEnergia == "") {
         alert("Selecione o Estado correspondente a sua localização")
     } else {
-    console.log(tarifaEnergia, "tarifa")
 
-    let consumo = (potenciaEquipamento * horasDeUso * usoDias / 1000).toFixed(2)
-    console.log(consumo, "consumo")
- 
-    let preço = (consumo * tarifaEnergia).toFixed(2)
+        let consumo = (potenciaEquipamento * horasDeUso * usoDias / 1000).toFixed(2)
+        let preço = (consumo * tarifaEnergia).toFixed(2)
 
-    console.log(preço, "preço")
-    msg.innerHTML = `<div class="retornoJS">
+        console.log(tarifaEnergia, "tarifa")
+        console.log(consumo, "consumo")
+        console.log(preço, "preço")
+        msg.innerHTML = `<div class="retornoJS">
     <p>Consumo mensal: ${consumo}</p> 
     <p>Custo mensal: ${preço}</p>
     </div>`
-}
+    }
 }
